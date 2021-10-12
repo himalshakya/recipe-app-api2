3453047a0d5909e16493beba1db92f7b2280e2b6
Himal Shakya <shakyahimal1@gmail.com>
2021-10-12 09:37:01 -0400

    Adding flake8 after pytest

--- app/tests/test_calc.py
+++ app/tests/test_calc.py
@@ -1,9 +1,9 @@
-import pytest
 from app import calc
 
+
 def test_add_numbers():
-  assert calc.add(10,20) == 30
+    assert calc.add(10, 20) == 30
 
 
 def test_subtract_numbers():
-  assert calc.subtract(20, 10) == 10
+    assert calc.subtract(20, 10) == 10
